b3c3e6b679e0ceba585b2744d680a75843923b22
Msendoo <97807733+Msendoo@users.noreply.github.com>
2024-08-17 13:37:16 -0600

    added funcitonality to get all the workouts

--- src/tracker.py
+++ src/tracker.py
@@ -34,4 +34,21 @@ class FitnessTracker:
         self.workouts.append(workout)
         return workout
     
+
+    def get_workouts(self, date=None):
+
+         """ 
+        Get's all workouts that occured on a date.
+        
+        :param date: Date is input/defined in the "YYYY-MM-DD" format, and returns all workouts
+         that occurs on that date. If date is input as none the workouts are returned for the current date
+        :return: activities is listed for the specfic date.
+        """
+         
+         if date is None:
+             date = datetime.now().strftime("%Y-%m-%d")
+         return [workout for workout in self.workouts if workout['date'] == date]
+         
+    
+    
      
